c8370ce3fdce3a057136f6e5314e383629766c53
unknown <tabea.arend@gmail.com>
2022-04-28 14:05:09 +0200

    BB-53: Assign input value to nickname-variable

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,32 +1,43 @@
 import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
-import logo from "../images/logo_binary_beasts.png";
 
 
 
 function Home(props) {
     let navigate = useNavigate();
     const [instructionsOpen, setInstructionsOpen] = useState(false); //for showing "Spielanleitung"
+    const [nickname, setNickname] = useState();
+
+    const updateName = (event) => { // changes name to the input value
+        setNickname(event.target.value);
+      };
 
     return(
         <div id="home-container" name="home-container">
             <h1>Home Page</h1>
             <div id="home-info" name="home-info">
                 <h1>Name der Software</h1>
-                <image src={logo}></image>
                 <p id="introduction" name="introduction">Kurze Einleitung</p>
             </div>
 
             <div id="user-login" name="user-login">
                 <label for="nickname">Nickname:</label>
-                <input id="nickname" name="nickname" type="text"></input>
-                <button onClick={()=> navigate("/leveloverview")}
+                <input id="nickname" name="nickname" type="text"  onChange={e => updateName(e)} ></input> 
+                <button onClick={()=> {
+                    navigate("/leveloverview");
+                    }
+                }
                     >Spielen!
                 </button>
             </div>
             <div id="description" name="description">
                 <button id="instruction" name="instruction" 
-                onClick={()=> {instructionsOpen ? setInstructionsOpen(false) : setInstructionsOpen(true)}} /* toggle button for how-to-play*/
+                onClick={(e)=> 
+                    {
+                        instructionsOpen ? setInstructionsOpen(false) : setInstructionsOpen(true); /* toggle button for how-to-play */
+                        
+                    } 
+                }
                   >Spielanleitung
                 </button>
                   {instructionsOpen &&
