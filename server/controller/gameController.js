c512795e55cc55826036a0b639bf9dc2e360f5b3
DanielSchl <daniel-schlecht01@web.de>
2022-04-28 08:07:28 +0200

    BB-72: Add getEvent Enpoint

--- server/controller/gameController.js
+++ server/controller/gameController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Level = require('../models/levelModel');
+const Event = require('../models/eventModel');
 
 const getLevel =  asyncHandler(async (req, res) => {
     try {
@@ -11,6 +12,16 @@ const getLevel =  asyncHandler(async (req, res) => {
     }
 })
 
+const getEvent =  asyncHandler(async (req, res) => {
+    try {
+        const event = await Event.find({ levelId: req.params.levelId });
+        console.log(event);
+        res.status(200).json({ event });
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+})
+
 module.exports = {
-    getLevel
+    getLevel, getEvent
 }
--- server/gateway/game.js
+++ server/gateway/game.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const controler = require("../controller/gameController");
-const {getLevel} = require("../controller/gameController");
+const {getLevel, getEvent} = require("../controller/gameController");
 const gateway = express.Router();
 
 // gateway.route('/getLevel').get(controler.getLevel);
 gateway.route('/getLevel').get(getLevel);
+
+gateway.route('/getEvent/:levelId').get(getEvent);
 
 module.exports = gateway;
