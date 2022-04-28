24ecf1dfe2c2e1764af930de669bb13ad54e1ffe
unknown <tabea.arend@gmail.com>
2022-04-28 09:18:08 +0200

    BB-53: Add toggle button for game instructions

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import logo from "../images/logo_binary_beasts.png";
 
+
+
 function Home(props) {
     let navigate = useNavigate();
+    const [instructionsOpen, setInstructionsOpen] = useState(false); //for showing "Spielanleitung"
 
     return(
-        <div id="home" name="home">
+        <div id="home-container" name="home-container">
             <h1>Home Page</h1>
             <div id="home-info" name="home-info">
-            <h1>Name der Software</h1>
-            <image src={logo}></image>
-            <p id="introduction" name="introduction">Kurze Einleitung</p>
+                <h1>Name der Software</h1>
+                <image src={logo}></image>
+                <p id="introduction" name="introduction">Kurze Einleitung</p>
             </div>
 
             <div id="user-login" name="user-login">
-            <label for="nickname">Nickname:</label>
-            <input id="nickname" name="nickname" type="text"></input>
-            <button onClick={()=> navigate("/leveloverview")}
-             >Spielen!
-            </button>
+                <label for="nickname">Nickname:</label>
+                <input id="nickname" name="nickname" type="text"></input>
+                <button onClick={()=> navigate("/leveloverview")}
+                    >Spielen!
+                </button>
+            </div>
+            <div id="description" name="description">
+                <button id="instruction" name="instruction" 
+                onClick={()=> {instructionsOpen ? setInstructionsOpen(false) : setInstructionsOpen(true)}} /* toggle button for how-to-play*/
+                  >Spielanleitung
+                </button>
+                  {instructionsOpen &&
+                      <div id="how-to-play" name="how-to-play">
+                          <h3>Spielanleitung</h3>
+                          <p>Es gibt Szenarien auf die du als </p>
+                      </div>
+                  }
             </div>
         </div>
     );
