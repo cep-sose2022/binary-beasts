fc678fd17231a19235930fab05fbfe498b5edb3c
Shamel <62669253+Shamel-hs@users.noreply.github.com>
2022-04-28 11:45:44 +0200

    BB-87: add functions to connect new endpoints

--- client/src/service.js
+++ client/src/service.js
@@ -10,7 +10,54 @@ const getLevel = (levelName) => {
     return xmlHttp.responseText;
 }
 
+/**
+ * get all levels from backend
+ * @returns {string}
+ */
+const getAllLevels = () => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", `http://localhost:5000/game/getAllLevels`, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+/**
+ * returns all events for one level
+ * @param levelId
+ * @returns {string}
+ */
+const getEvent = (levelId) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", `http://localhost:5000/game/getEvent/${levelId}`, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+/**
+ * returns all cards for one event
+ * @param eventId
+ * @returns {string}
+ */
+const getEventCard = (eventId) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", `http://localhost:5000/game//getEventCard/${eventId}`, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+/**
+ * returns a card by cardId
+ * @param cardId
+ * @returns {string}
+ */
+const getCard = (cardId) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", `http://localhost:5000/game/getCard/${cardId}`, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
 
 module.exports = {
-    getLevel,
+    getLevel, getAllLevels, getEvent, getEventCard, getCard
 }
