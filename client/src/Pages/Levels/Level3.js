1e14d02100e03d7d1013f7d442a427c925fb3e9c
DanielSchl <daniel-schlecht01@web.de>
2022-05-05 15:06:30 +0200

    BB-64: Add Patchmanagement-Level logic

--- client/src/Pages/Levels/Level3.js
+++ client/src/Pages/Levels/Level3.js
@@ -1,57 +1,127 @@
 import React, {useState} from "react";
+import service from './../../service';
+import {useNavigate} from 'react-router-dom';
 import "../../index.css";
 
 function Level3() {
-  const [data, setData] = React.useState(null);
-  const [eventText, setEventText] = useState(null);
-  const [showScore, setShowScore] = useState(false)
-  const [score, setScore] = useState(0);
-  const [currentEvent, setCurrentEvent] = useState(1);
-  //const [currentImage, setCurrentImage] = useState(mail);
+    let navigate = useNavigate();
+    const level = service.getLevel('level3');
+    const [currentEvent, setCurrentEvent] = useState(1);
 
-  const handleAnswerButtonClick = (cardOption) => {
-    /* if (cardOption.nextEvent === 0){
-      setShowScore(true);
+    const [currentCards, setCurrentCards] = useState(level.level.events[0].cards);
+    const [eventText, setEventText] = useState(level.level.events[0].text[0]);
+    const [eventTextNumber, setEventTextNumber] = useState(0);
+    const [currentImage, setCurrentImage] = useState(null);
 
+    const [usbInfected, setUsbInfected] = useState(false);
+    const [compatibilityChecked, setCombatibilityChecked] = useState(false);
+    const [dataSaved, setDataSaved] = useState(false);
+    const [patchDocumented, setPatchDocumented] = useState(false);
+    const [cardSevenPlayed, setCardSevenPlayed] = useState(false);
+    const [cardElevenPlayed, setCardElevenPlayed] = useState(false);
+    const [gameOver, setGameOver] = useState(false);
+
+    React.useEffect(() => {
+      //setEventText(level.level.events[currentEvent - 1].text[eventTextNumber]);
+      if(currentEvent == 3 && patchDocumented) {
+        setCurrentCards(level.level.events[currentEvent - 1].cards.filter(card => card.name != 'card10'));
+      } else {
+        setCurrentCards(level.level.events[currentEvent - 1].cards);
+      }
+    }, [currentEvent]);
+
+    React.useEffect(() => {  
+      if(usbInfected && cardSevenPlayed) {
+        setEventText(level.level.events[currentEvent - 1].text[4]);
+        setCardSevenPlayed(false)
+        setUsbInfected(false);
+      } else if(!compatibilityChecked && usbInfected && dataSaved && currentEvent == 3 ) {
+        setEventText(level.level.events[currentEvent - 1].text[9]);
+        setCombatibilityChecked(true);
+      } else if(!compatibilityChecked && usbInfected && currentEvent == 3 ) {
+        setEventText(level.level.events[currentEvent - 1].text[3]);
+        setCombatibilityChecked(true);
+      } else if(!compatibilityChecked && dataSaved && currentEvent == 3 ) {
+        setEventText(level.level.events[currentEvent - 1].text[10]);
+        setCombatibilityChecked(true);
+      }else if(usbInfected && currentEvent == 3)  {
+        setEventText(level.level.events[currentEvent - 1].text[1]);
+        setUsbInfected(false);
+      } else if(!compatibilityChecked && currentEvent == 3 ) {
+        setEventText(level.level.events[currentEvent - 1].text[2]);
+        setCombatibilityChecked(true);
+      } else if(cardElevenPlayed && !patchDocumented ) {
+        setEventText(level.level.events[currentEvent - 1].text[6]);
+        setCardElevenPlayed(false);
+      } else {
+        setEventText(level.level.events[currentEvent - 1].text[eventTextNumber]);
+        setCardSevenPlayed(false);
+      } 
+
+      if(gameOver) {
+        setCurrentCards(currentCards.filter(card => false));
+      }
+    }, [eventTextNumber, currentEvent]);
+
+    const handleAnswerButtonClick = (cardOption) => {
+        setCurrentEvent(cardOption.nextEvent);
+        setEventTextNumber(cardOption.nextEventText);
+        setCurrentCards(currentCards.filter(card => card.name != cardOption.name));
+
+        if(cardOption.name == 'card4') {
+          setCombatibilityChecked(true);
+        }
+
+        if(cardOption.name == 'card6') {
+          setUsbInfected(true);
+        }
+          
+        if(cardOption.name == 'card7') {
+          setCardSevenPlayed(true);
+        }
+
+        if(cardOption.name == 'card8') {
+          setDataSaved(true);
+        }
+
+        if(cardOption.name == 'card9') {
+          setPatchDocumented(true);
+        }
+
+        if(cardOption.name == 'card11') {
+          setCardElevenPlayed(true);
+        }
+
+        if(cardOption.name == 'card12' || cardOption.name == 'card13') {
+          setGameOver(true);
+        }
+
+    }
+
+    const eventTextSplit = () => {
+      //Bei jedem '<' im eventText wird ein br-Tag eingebaut
+      const wrapped = eventText.split('<').reduce((prev, cur) => [ ...prev, cur, <br /> ], [])
+      return wrapped;
     };
-    setCurrentEvent(cardOption.nextEvent);
-    setScore(score + cardOption.points);
-    if ( cardOption.nextImage === 'mail' ){
-      setCurrentImage(mail);
-    }
-    else if (cardOption.nextImage === 'login'){
-      setCurrentImage(login);
-    }
-    else if (cardOption.nextImage === 'download'){
-      setCurrentImage(download);
-    } */
-  }
 
-  return (
-      <div className='app'>
-        {showScore ? (
-            <div className='score-section'>
-              Score: {score}
-            </div>
-        ) : (
+    return (
+        <div className='app'>
             <>
-              <div className='answers'>
                 <div className='images'>
-                  <img className='img'/>
+                    <img src={currentImage} className='img'/>
                 </div>
                 <div className='cards'>
-                  {!data ? "Loading..." :data.map((cardOption) => (
-                      <button onClick={()=> handleAnswerButtonClick(cardOption)}>{cardOption.text}</button>
-                  ))}
+                    {!currentCards ? "Loading..." : currentCards.map((cardOption) => (
+                        <button onClick={() => handleAnswerButtonClick(cardOption)}>{cardOption.text}</button>
+                    ))}
                 </div>
-              </div>
-              <div className='questions'>
-                <br/><div className='event-text'>{!eventText ? "Loading..." :eventText}</div>
-              </div>
+                <div className='questions'>
+                    <br/>
+                    <div className='event-text'>{!eventText ? "Loading..." : eventTextSplit()}</div>
+                </div>
             </>
-        )}
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Level3;
