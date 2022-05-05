f50e49602169cc1d7d791359358726eecdcd52ab
Shamel <62669253+Shamel-hs@users.noreply.github.com>
2022-05-05 11:47:56 +0200

    BB-101: Add new functions to set & get score

--- client/src/library/bib.js
+++ client/src/library/bib.js
@@ -1,3 +1,4 @@
+const service = require('./../service');
 const lib = {};
 
 /**
@@ -14,6 +15,51 @@ lib.setNickname = (name) => {
  */
 lib.getNickname = () => {
     return localStorage.getItem('username');
+};
+
+/**
+ * set score for user
+ * should be used every time a level is started
+ * @param levelToken
+ */
+lib.setLevelStartScore = (levelToken) => {
+    const scores = service.getScores();
+    let userScore = 0;
+
+    // check if user has a score in database using service
+    for (let i = 0; i < scores.sortedUsers.length; i++) {
+        if (scores.sortedUsers[i].username === lib.getNickname()) {
+            userScore = scores.sortedUsers[i].score;
+        }
+    }
+
+    // check if user has a score for THIS level in database using service
+    const level = service.getLevel(levelToken);
+    const leaderBoard = service.getUserLeaderboard(lib.getNickname());
+    for (let i = 0; i < leaderBoard.userLeaderboard.length; i++) {
+        if(leaderBoard.userLeaderboard[i].levelId === level.level._id) {
+            userScore -= leaderBoard.userLeaderboard[i].score;
+        }
+    }
+    localStorage.setItem('score', userScore);
+};
+
+/**
+ * update score in level
+ * @param points
+ */
+lib.updateScore = (points) => {
+    let temp = parseInt(localStorage.getItem('score'));
+    temp += points;
+    localStorage.setItem('score', temp);
+};
+
+/**
+ * get current score
+ * @returns {number}
+ */
+lib.getScore = () => {
+    return parseInt(localStorage.getItem('score'));
 };
 
 module.exports = lib;
