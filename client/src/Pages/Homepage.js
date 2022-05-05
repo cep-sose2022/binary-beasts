154f9340b1057059a23afa869d81d0648c5a8814
unknown <tabea.arend@gmail.com>
2022-05-05 17:31:46 +0200

    BB-105: Resolve merge conflict with homepage

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import '../App.css';
-<<<<<<< HEAD
 import lib from "../library/bib.js";
 import Navbar from "../Navbar";
 import Footer from "./subpages/Footer";
@@ -25,25 +24,6 @@ function Home(props) {
     }
     else 
       setInputMessage(2); 
-=======
-import {useCookies} from "react-cookie";
-
-import Footer from "./Footer";
-
-
-function Home(props) {
-  let navigate = useNavigate();
-  let textInput = React.createRef(); //create a reference
-  const [instructionsOpen, setInstructionsOpen] = useState(false); //for showing "Spielanleitung"
-  const [nickname, setNickname] = useState();
-  const [cookies, setCookie] = useCookies("user");
-
-    function updateName(event){  // changes name to the input value
-        setNickname(textInput.current.value);
-        setCookie("user", textInput.current.value, {path: "/"}); //writes input into cookie
-        console.log(cookies.user);
-    }
->>>>>>> 1056b74 (BB-57: Create simple footer without links)
 
   }
 
@@ -57,7 +37,6 @@ function Home(props) {
         <p id="introduction" name="introduction">Kurze Einleitung</p>
       </div>
 
-<<<<<<< HEAD
       <div id="user-login" name="user-login">
         <label for="nickname">Nickname:</label>
 
@@ -97,36 +76,6 @@ function Home(props) {
     <Footer/>
     </>
   );
-=======
-                <input id="nickname" name="nickname" ref={textInput} type="text"></input><br/>
-            </div>
-            <div id="play"><button className="playbutton"onClick={(e)=> {updateName(e); navigate("/leveloverview")}}
-                    >Spielen
-            </button></div>
-            
-                <button id="instructionbutton" name="instruction" 
-                onClick={()=> {instructionsOpen ? setInstructionsOpen(false) : setInstructionsOpen(true)}} /* toggle button for how-to-play*/
-                  >Spielanleitung
-                </button>
-                <div id="description">
-                  {instructionsOpen &&
-                        <div id="how-to-play" name="how-to-play">
-                          <h3>Spielanleitung</h3>
-                          <p>Es gibt unterschiedliche Szenarien, die Sie durchlaufen werden. 
-                          Sie haben dabei freie Wahl, welches Sie wählen, aber wenn Sie eins begonnen haben, 
-                          müssen Sie es erst zu Ende spielen, bevor Sie andere spielen können.
-                          In jedem Szenario kommen Ereignisse auf die reagieren müssen, indem Sie Karten ausspielen.
-                          Dabei müssen Sie beachten, dass Sie jede Karte nur einmal benutzen können. 
-                          Haben Sie sich entschieden wie Sie auf das Ereignis reagieren und eine Karte gewählt, folgt das nächste Ereignis mit neuen Karten.
-                          Bei manchen Szenarien bleiben die alten Karten erhalten, aber darauf sollten Sie sich nicht verlassen.
-                           </p>
-                      </div>
-                  }
-                  </div>
-                  <Footer />
-        </div>
-    );
->>>>>>> 1056b74 (BB-57: Create simple footer without links)
 }
 
 export default Home;
