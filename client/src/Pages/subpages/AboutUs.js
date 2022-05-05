35f26eccd2d025822306c7e9c7ea61687f12f021
unknown <tabea.arend@gmail.com>
2022-05-05 20:45:08 +0200

    BB-105: Fill footer pages with content

--- client/src/Pages/subpages/AboutUs.js
+++ client/src/Pages/subpages/AboutUs.js
@@ -4,8 +4,15 @@ function AboutUs(){
     return(
         <div id="about-us-container" name="about-us-container">
             <h1>Über uns</h1>
-            <h3>Warum wir so cool sind.</h3>
-            <h2>Kontakt</h2>
+            <h2>Wer wir sind</h2>
+            <p>
+                Binary Beasts ist ein Team, bestehend aus Studierenden der Hochschule Mannheim, 
+                welches das Ziel hat in der Industriebranche mehr Aufmerksamkeit auf Cyber Security relevante Themen zu lenken.
+                In der Industrie 4.0 wird man früher oder später darauf stoßen, weil es einen immer größeren Anteil einnimmt. 
+                Wir möchten vermeiden, dass Unternehmen Opfer von Cyber Angriffen werden, 
+                indem wir mit dieser Webanwendung präventiv darauf aufmerksam machen wo welche Schwachstellen liegen und wie man sie beheben kann.
+            </p>
+            <h3>Kontakt</h3>
             <p>binarybeasts100@gmail.com</p>
         </div>
     );
--- client/src/Pages/subpages/FAQ.js
+++ client/src/Pages/subpages/FAQ.js
@@ -6,12 +6,25 @@ function FAQ(){
             <h1>FAQ</h1>
             <h2>Spiel</h2>
             <h3>Wie funktioniert es?</h3>
+                <p>
+                    Einfach einen Benutzernamen angeben (aber nicht den echten Namen!) und auf "Spielen" klicken.
+                </p>
+            <h3>Kann ich ein Level wiederholen?</h3>
+                <p>Ja, aber es wird nur die bessere Punktzahl gespeichert</p>
+            <h3>Gibt es einen Lösungsweg?</h3>
+                <p>Jap.</p>
+            <h3>Kann man ein Level pausieren?</h3>
+                <p>Noch nicht. Eventuell später.</p>
             <h2>Einstellungen</h2>
-            <h3>Wie kann ich die Farbe ändern?</h3>
-            <p>Gar nicht.</p>
+            <h3>Wie kann ich die Farbe anpassen?</h3>
+                <p>Gar nicht.</p>
+            <h3>Kann ich meinen Benutzernamen nachträglich ändern?</h3>
+                <p>Nein.</p>
             <h2>Benutzerprofil</h2>
             <h3>Wer kann meinen Spielstand sehen.</h3>
-            <p>Nur du. Und wir vielleicht.</p>
+                <p>Nur du. Und wir vielleicht.</p>
+            <h3>Wer sieht meine Punkte?</h3>
+                <p>Jeder. Auf dem Leaderboard.</p>
         </div>
     );
 }
