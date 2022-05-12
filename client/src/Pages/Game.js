cc5def0e0a27ee4c1dd0a2f94ab598ece1fbd0e2
unknown <tabea.arend@gmail.com>
2022-05-12 10:57:18 +0200

    BB-60: Remove unnecessary code

--- client/src/Pages/Game.js
+++ client/src/Pages/Game.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Level1 from "./Levels/Level1";
 import Level2 from "./Levels/Level2"
 import Level3 from "./Levels/Level3";
+<<<<<<< HEAD
 import service from '../service';
 import lib from '../library/bib.js';
 
@@ -66,6 +67,57 @@ function Game(){
                 <p>Score: <span>{lib.getScore()}</span></p>
             </div>   
         </nav>
+=======
+import service from "../service";
+import lib from "../library/bib";
+
+function Game(props) {
+    const location = useLocation();
+    const startScore = lib.getScore();
+    let navigate = useNavigate(); 
+
+    // Timer
+    const { startingMinutes = 4, startingSeconds = 0 } = 4;
+    const [mins, setMinutes] = useState(startingMinutes);
+    const [secs, setSeconds] = useState(startingSeconds);
+    useEffect(() => {
+        let sampleInterval = setInterval(() => {
+            if (secs > 0) {
+                setSeconds(secs - 1);
+            }
+            if (secs === 0) {
+                if (mins === 0) {
+                    const dif = lib.getScore() - startScore;
+                    service.postUserLeaderboard(lib.getNickname(), level.level._id, dif);
+                    navigate('./../LevelOverview');
+                } else {
+                    setMinutes(mins - 1);
+                    setSeconds(59);
+                }
+            }
+        }, 1000);
+        return () => {
+            clearInterval(sampleInterval);
+        };
+    });
+    // end of Timer  
+
+    return (
+        <div className='app'>
+        <>
+            <nav className="gameNavbar">
+                <div id="navlevelround" className="navgamecontent">
+                    <p>Level: <span>{location.state.levelid}</span> </p>
+                </div>
+                <div className="navgamecontent">
+                    <p>Time: <span>{mins}:{secs < 10 ? `0${secs}` : secs}</span> </p>
+                </div>
+                <div id="score" className="navgamecontent">
+                    <p>Score: <span>{lib.getScore()}</span></p>
+                </div>
+            </nav>
+     
+>>>>>>> 02ed1b4 (BB-60: Remove unnecessary code)
 
         {location.state.levelid === 1 && <Level1 />}
         {location.state.levelid === 2 && <Level2 />}
