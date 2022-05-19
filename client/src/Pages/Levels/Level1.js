469cc142db19ec317bf8ee5891cc7a62b41b6b0f
Shamel <62669253+Shamel-hs@users.noreply.github.com>
2022-05-19 10:45:39 +0200

    BB-157: Rework level1

--- client/src/Pages/Levels/Level1.js
+++ client/src/Pages/Levels/Level1.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import mail from './../../images/level1/mail.jpg';
 import login from './../../images/level1/login.jpg';
 import download from './../../images/level1/download.jpg';
+import ransomware from './../../images/level1/ransomware.jpg';
 import service from './../../service';
 import { useNavigate } from 'react-router-dom';
 import lib from '../../library/bib.js';
@@ -14,9 +15,6 @@ lib.setLevelStartScore('level1');
 const level = service.getLevel('level1');
 
 function Level1(props) {
-
-    // save start score
-    const startScore = lib.getScore();
 
     let navigate = useNavigate();
 
@@ -51,12 +49,18 @@ function Level1(props) {
             setCurrentImage(login);
         } else if (cardOption.nextImage === 'download') {
             setCurrentImage(download);
-        } else if (cardOption.nextEvent === 0) {
-            const dif = lib.getScore() - startScore;
-            service.postUserLeaderboard(lib.getNickname(), level.level._id, dif);
+        } else if (cardOption.nextImage === 'ransomware') {
+            setCurrentImage(ransomware);
+        }
+        else if (cardOption.nextEvent === 0) {
+            if (lib.getScore() >= 0) {
+                service.postUserLeaderboard(lib.getNickname(), level.level._id, lib.getScore());
+            } else if (lib.getScore() < 0) {
+                service.postUserLeaderboard(lib.getNickname(), level.level._id, 0);
+            }
             // save level number and feedback in local storage and navigate to win page
             localStorage.setItem('levelNumber', '1');
-            localStorage.setItem('feedback', level.level.events[3].text[1]);
+            localStorage.setItem('feedback', level.level.events[5].text[1]);
             navigate('../win');
         }
     }
