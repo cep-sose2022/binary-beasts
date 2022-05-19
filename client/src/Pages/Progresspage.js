d38486a53d31c1d0bcadc543f1f4affbfe63275b
Shamel <62669253+Shamel-hs@users.noreply.github.com>
2022-05-19 22:07:18 +0200

    BB-163: Auto-refresh for pages

--- client/src/Pages/Progresspage.js
+++ client/src/Pages/Progresspage.js
@@ -3,15 +3,20 @@ import service from "../service";
 import lib from "../library/bib.js";
 import Navbar from "../Navbar";
 
-const allLevels = service.getAllLevels().allLevels;
-const userBoard = service.getUserLeaderboard(lib.getNickname()).userLeaderboard; 
+function Progress(props) {
+    const allLevels = service.getAllLevels().allLevels;
+    const userBoard = service.getUserLeaderboard(lib.getNickname()).userLeaderboard;
 
-function Progress() {
+    //determine the percentage of the finished levels
+    function calcProgress(){
+        return (
+            <p>{Math.round(userBoard.length/allLevels.length * 100)} %</p>
+        )
+    }
     return(
         <>
         <Navbar />
         <div id="progress-container" className="container">
-            {console.log(lib.getNickname())}
             <div id="info" className="box">
                 <h1>Fortschritt - {lib.getNickname()}</h1>
             
@@ -21,46 +26,31 @@ function Progress() {
             </div>
             <div id="level-scores">
                 <h2>Level-Überblick</h2>
-                {displayLevelScores()}
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Level</th>
+                        <th>Punkte</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        userBoard.map(element => {
+                            return(
+                                <tr key={element}>
+                                    <td>{service.getLevelName(element.levelId).levelName}</td>
+                                    <td>{element.score}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
             </div>
             </div>
         </div>
         </>
     );
-}
-
-
-//determine the percentage of the finished levels
-function calcProgress(){
-    return (
-        <p>{Math.round(userBoard.length/allLevels.length * 100)} %</p>
-    )
-}
-
-// returns table with the levelname and the corresponding score
-function displayLevelScores(){
-    return(
-        <table>
-            <thead>
-            <tr>
-                <th>Level</th>
-                <th>Punkte</th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-            userBoard.map(element => {
-                return(
-                    <tr key={element}>
-                    <td>{service.getLevelName(element.levelId).levelName}</td>
-                    <td>{element.score}</td>
-                    </tr>
-               )
-            })
-            }
-            </tbody>
-        </table>
-    )
 }
 
 
