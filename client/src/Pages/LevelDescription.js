74f193dd6e53b3429e9f9f47376c8ee1ebdd3861
Shamel <62669253+Shamel-hs@users.noreply.github.com>
2022-05-20 08:32:20 +0200

    BB-181: Auto-refresh for pages

--- client/src/Pages/LevelDescription.js
+++ client/src/Pages/LevelDescription.js
@@ -1,13 +1,13 @@
 import React from "react";
 import service from './../service';
 import { useLocation, useNavigate } from "react-router-dom";
+const levels = service.getAllLevels();
 
 function Leveldescription() {
     let navigate = useNavigate();
     let location = useLocation();
   
     const levelNumber = location.state.levelid;
-    const levels = service.getAllLevels();
     const level = levels.allLevels.filter(level => level.token === "level" + levelNumber);
     return(
       <div id="description-container" className="container">
@@ -16,18 +16,16 @@ function Leveldescription() {
           <div id="level-description" className="box">
             <p>{level[0].description}</p>
           </div>
-          
-          
-          
+
           <button id="levelOverview-button" onClick={() => {
-            navigate("/game", {
+            navigate("../game", {
               state: {
                 levelid: levelNumber
               }
             });
           }}>Starten</button>
           <button id="levelOverview-button" onClick={() => {
-            navigate("/leveloverview");
+            navigate("../leveloverview");
           }}>Zurück</button>
       </div>
       
