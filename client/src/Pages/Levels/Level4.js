0131c0c2325c215582e6db3ee551b03b59e0ca06
unknown <tabea.arend@gmail.com>
2022-05-23 14:47:00 +0200

    BB-119: Consider exceptional rules for isolatedNetwork-card

--- client/src/Pages/Levels/Level4.js
+++ client/src/Pages/Levels/Level4.js
@@ -5,13 +5,16 @@ import lib from "../../library/bib";
 
 
 const level = service.getLevel('level4');
-const cardsPlayed = [];
 
 let encryptDrive = false;
 let isolatedNetwork = false;
 let whitelisting = false;
 let physicalLocks = false;
-const nextEventHistory = [1]; //because event0 does not have "nextEvent"
+const cardsPlayed = [];
+const nextEventHistory = [1]; //initially with 1 because event0 does not have "nextEvent"
+
+let isLevelNameSet = false;
+let isScanned = false;
 
 function Level4(props) { //external devices
     let navigate = useNavigate();
@@ -21,20 +24,36 @@ function Level4(props) { //external devices
     const [eventTextNumber, setEventTextNumber] = useState(0);
     let startScore = lib.getScore();
 
+    let dynEvent;
+    let dynEventText;
+
+
+    if (!isLevelNameSet) {
+        props.passLevelName(level.level.name);
+        isLevelNameSet = true;
+    }
 
     React.useEffect(() => {
         setEventText(level.level.events[currentEvent - 1].text[eventTextNumber]); //-1 because eventName and eventNumber differ by 1
-        setCurrentCards(level.level.events[currentEvent - 1].cards.filter(card => !cardsPlayed.includes(card.name)));
-        console.log("history (useEffect): " + nextEventHistory + " - currenEvent: " + currentEvent);
+        if (currentEvent === 1) //== baseEvent
+            setCurrentCards(level.level.events[currentEvent - 1].cards.filter(card => !cardsPlayed.includes(card.name)));
+        else if (!isolatedNetwork)
+            setCurrentCards(level.level.events[currentEvent - 1].cards.filter(card => card.name != ("card10" || "card21"))); // two events have one card less
+        else
+            setCurrentCards(level.level.events[currentEvent - 1].cards);
+            console.log("history (useEffect): " + nextEventHistory + " - currenEvent: " + currentEvent);
     }, [currentEvent]);
 
     const handleAnswerButtonClick = (cardOption) => {
-        if(currentEvent === 1){ 
+        if (currentEvent === 1) {
             handleBaseEvent(cardOption);
-        } else if (cardOption.name === "card0"){
+        } else if (cardOption.name === "card0") {
             determineNextEvent(cardOption);
+        } else if (cardOption.name === "card17") {
+
         } else {
             nextEventHistory.push(cardOption.nextEvent); //add nextEvent to history
+            checkExceptions(cardOption);
             setEventTextNumber(cardOption.nextEventText);
             setCurrentEvent(cardOption.nextEvent);
         }
@@ -47,17 +66,24 @@ function Level4(props) { //external devices
      * when card0 is clicked a different event is rendered depeding on the previous events
      */
     const determineNextEvent = (cardOption) => {
-        let dynEvent = nextEventHistory[nextEventHistory.length - 1] ; //get the next event from history
+        dynEvent = nextEventHistory[nextEventHistory.length - 1]; //get the next event from history
+        dynEventText = 0; //all events that follow card0 only have one text
         console.log("dynEvent (before): " + dynEvent);
         if (currentEvent === 1) { //basic event with security meassures 
-            dynEvent = nextEventHistory[nextEventHistory.length - 1]+1; 
+            dynEvent = nextEventHistory[nextEventHistory.length - 1] + 1;
         } else if (currentEvent === dynEvent) { //
-            dynEvent = 1; 
+            dynEvent = 1;
+        } else if (currentEvent === 13) {
+            endGame();
+        } else if (cardOption.name === "card17") {
+            dynEvent = currentEvent; //reference to the same event 
+            isScanned = true; //TODO exclude option, of event9 text 0
         }
         else {
+            checkExceptions(cardOption);
             console.log("something went wrong");
         }
-        setEventTextNumber(0); //all events that follow card0 only have one text
+        setEventTextNumber(dynEventText);
         setCurrentEvent(dynEvent);
         console.log("dynEvent (after): " + dynEvent);
     }
@@ -71,18 +97,54 @@ function Level4(props) { //external devices
             physicalLocks = true;
         } else if (cardOption.name === "card4") {
             isolatedNetwork = true;
-        } else
+        } else //card5
             ;
 
         cardsPlayed.push(cardOption.name);
-
         determineNextEvent(cardOption);
     }
 
+    const checkExceptions = (cardOption) => {
+        if (cardOption.name === "card10" || cardOption.name === "card21") {
+            //check for isolatedNetwork -> set this card visible or not
+            if (!isolatedNetwork)
+                setCurrentCards(level.level.events[currentEvent - 1].cards.filter(card => cardOption.name != ("card10" || "card21")));
+        } else if (cardOption.name === "card17") {
+            isScanned = true;
+        } else if (cardOption.name === "card16") {
+            if (isScanned) {
+                if (whitelisting)
+                    dynEventText = 5;
+                else
+                    dynEventText = 4;
+            }
+        } else if (cardOption.name === "card18") {
+            if (isScanned) {
+                if (whitelisting)
+                    dynEventText = 5;
+            }
+        } else if (cardOption.name === "card13") {
+            //if encrypted show eventText0 else show eventText1
+            if (encryptDrive)
+                dynEventText = 0;
+            else
+                dynEventText = 1;
+        } else if (cardOption.name === "card14") {
+            //if !physicalLock show eventText2 else show eventText3
+            if (physicalLocks) {
+                dynEventText = 3;
+            } else {
+                dynEventText = 2;
+            }
+        }
+
+    };
+
     const endGame = () => {
-        const dif = lib.getScore() - startScore;
-        service.postUserLeaderboard(lib.getNickname(), level.level._id, dif);
-        navigate('./../LevelOverview');
+        //  const dif = lib.getScore() - startScore;
+        //  service.postUserLeaderboard(lib.getNickname(), level.level._id, dif);
+        localStorage.setItem('levelNumber', '1');
+        navigate('../win');
     }
 
     return (
