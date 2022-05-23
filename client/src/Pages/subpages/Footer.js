8dfcf125323d481f7902c3777a4d233b7a93d725
DanielSchl <daniel-schlecht01@web.de>
2022-05-23 07:43:12 +0200

    BB-146: Add MIT-License on Footer & PirvacyPolicy

--- client/src/Pages/subpages/Footer.js
+++ client/src/Pages/subpages/Footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
     return (
         <footer>
             <ul id="info-links" name="info-links">
-                <p>&copy; 2022 Binary Beasts. All rights reserved.</p>
+                <p>&copy; Copyright 2022 Binary Beasts</p>
                 <li><NavLink activeclassname="active" to="/aboutus">Über uns</NavLink></li>
                 <li><NavLink activeclassname="active" to="/faq">FAQ</NavLink></li>
                 <li><NavLink activeclassname="active" to="/privacy_policy">Impressum &amp; Datenschutz</NavLink></li>
--- client/src/Pages/subpages/PrivacyPolicy.js
+++ client/src/Pages/subpages/PrivacyPolicy.js
@@ -1,39 +1,57 @@
 import React from "react";
 
-function PrivacyPolicy(){
-    return(
+function PrivacyPolicy() {
+    return (
         <div id="policy-container" name="policy-container">
             <h1>Impressum</h1>
-                <p>
-                    Binary Beasts <br/> 
-                    Speyerer Straße 2 <br/>
-                    68163 Mannheim
-                    binarybeasts100@gmail.com
-                </p>
-            <hr/>
+            <p>
+                Binary Beasts <br />
+                Speyerer Straße 2 <br />
+                68163 Mannheim
+                binarybeasts100@gmail.com
+            </p>
+            <hr />
             <h2>Rechtliche Hinweise</h2>
             <p>
-                Die Inhalte dieser Website sind geschützt und nur für Personen mit der erteilten Berechtigung zugänglich.
-                Es ist untersagt jegliche Inhalte zu vervielfältigen oder nicht authorisierten Personen zugänglich zu machen.
+                Diese Website verwendet eine MIT-Lizenz:
+            </p>
+            <p>
+                Permission is hereby granted, free of charge, to any person obtaining a copy
+                of this software and associated documentation files (the "Software"), to deal
+                in the Software without restriction, including without limitation the rights
+                to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                copies of the Software, and to permit persons to whom the Software is
+                furnished to do so, subject to the following conditions:
+                <br />
+                The above copyright notice and this permission notice shall be included in all
+                copies or substantial portions of the Software.
+                <br />
+                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                SOFTWARE.
             </p>
             <h3>Datenschutz</h3>
-                <p>
-                    Die Nutzerdaten die wir speichern sind:
-                    <ul>
-                        <li>Nutzername</li>
-                        <li>Spielfortschritt</li>
-                        <li>erreichte Punkte mit dazugehörigem Level</li>
-                    </ul>
-                    Die Löschung von Nutzerdaten ist nur durch Kontaktaufnahme mit Binary Beasts möglich.
-                </p>
-                <p>
-                    Um den Spielstand individuell anzuzeigen sind Speicherungen auf dem Endnutzergerät nötig, 
-                    die vom Nutzer selbst gelöscht werden können. Dafür übernimmt Binary Beasts keinerlei Haftung. 
-                </p>
+            <p>
+                Die Nutzerdaten die wir speichern sind:
+                <ul>
+                    <li>Nutzername</li>
+                    <li>Spielfortschritt</li>
+                    <li>erreichte Punkte mit dazugehörigem Level</li>
+                </ul>
+                Die Löschung von Nutzerdaten ist nur durch Kontaktaufnahme mit Binary Beasts möglich.
+            </p>
+            <p>
+                Um den Spielstand individuell anzuzeigen sind Speicherungen auf dem Endnutzergerät nötig,
+                die vom Nutzer selbst gelöscht werden können. Dafür übernimmt Binary Beasts keinerlei Haftung.
+            </p>
             <h3>Urheberrecht</h3>
-                <p>
-                    Alle Inhalte dieser Website sind geistiges Eigentum und dürfen nicht vervielfältigt oder kopiert werden.
-                </p>
+            <p>
+                Alle Inhalte dieser Website sind geistiges Eigentum und dürfen nicht vervielfältigt oder kopiert werden.
+            </p>
         </div>
     );
 }
