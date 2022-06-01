106ded1206cf08959cbc6c1d4d6e1489c809da89
Shamel <62669253+Shamel-hs@users.noreply.github.com>
2022-06-01 16:16:05 +0200

    BB-215: Add new imports to cardImages

--- client/src/library/cardImages.js
+++ client/src/library/cardImages.js
@@ -1,3 +1,6 @@
+let alert =  require('./../images/cardImages/alert.png');
+let boss =  require('./../images/cardImages/boss.png');
+let browse =  require('./../images/cardImages/browse.png');
 let deleted =  require('./../images/cardImages/deleted.png');
 let devices =  require('./../images/cardImages/devices.png');
 let email =  require('./../images/cardImages/email.png');
@@ -6,13 +9,16 @@ let humanInterface =  require('./../images/cardImages/humanInterface.png');
 let mouseAction =  require('./../images/cardImages/mouseAction.png');
 let network =  require('./../images/cardImages/network.png');
 let orga =  require('./../images/cardImages/orga.png');
+let phone =  require('./../images/cardImages/phone.png');
 let technical =  require('./../images/cardImages/technical.png');
-
 
 const ci = {};
 
 ci.getCardImage = (imageName) => {
-    if(imageName === 'deleted') return deleted;
+    if(imageName === 'alert') return alert;
+    else if (imageName === 'boss') return boss;
+    else if (imageName === 'browse') return browse;
+    else if (imageName === 'deleted') return deleted;
     else if (imageName === 'devices') return devices;
     else if (imageName === 'email') return email;
     else if (imageName === 'encryption') return encryption;
@@ -20,6 +26,7 @@ ci.getCardImage = (imageName) => {
     else if (imageName === 'mouseAction') return mouseAction;
     else if (imageName === 'network') return network;
     else if (imageName === 'orga') return orga;
+    else if (imageName === 'phone') return phone;
     else if (imageName === 'technical') return technical;
 };
 
